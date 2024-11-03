3624b1f6cd8f1499af62cbefc662eb82892958cd
Shrestha Hardia <84022759+hardiashrestha@users.noreply.github.com>
2024-11-03 12:10:22 +0000

    Images changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
             <Link to='/' className='logo'>
                 <img src={logo} alt="Home" style={{ width: '105px', height: '97.22px', position: 'absolute', left: '3.06px', top: '4.56px' }} />
             </Link>
-            <Link to='/about-us' className='button about_us'>ABOUT US</Link>
+            <Link to='/about-us' className='button about_us'>TEAM</Link>
             <Link to='/join-us' className='button join_us'>JOIN US</Link>
             <Link to='/events' className='button events'>EVENTS</Link>
             <Link to='/contact' className='button contact'>CONTACT</Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
-import logo from '../assets/STEM-INSPIRES-Home.png';
-import image1 from '../assets/Home-img-2.png'; 
-import image2 from '../assets/Home-img-3.png'; 
-import image3 from '../assets/Home-img-4.png'; 
+import image1 from '../assets/image1.png'; 
+import image2 from '../assets/image2.png'; 
+import image3 from '../assets/image3.png'; 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -27,9 +26,6 @@ const Home = () => {
 
             <Slider {...settings} style={{ width: '475px', height: '480.22px', position: 'absolute', left: '885.06px', top: '100.56px' }}>
                 <div>
-                    <img src={logo} alt="Home" style={{ width: '475px', height: '480.22px' }} />
-                </div>
-                <div>
                     <img src={image1} alt="Slide 1" style={{ width: '475px', height: '480.22px' }} />
                 </div>
                 <div>
@@ -40,7 +36,7 @@ const Home = () => {
                 </div>
             </Slider>
 
-            <Link to="/about-us" className="button about-us">ABOUT US</Link>
+            <Link to="/about-us" className="button about-us">TEAM</Link>
             <Link to="/contact" className="button contact-us">CONTACT</Link>
 
         </div>
