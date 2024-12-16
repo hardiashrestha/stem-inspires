655d745d4b03e7989118f987eda3147e8e67210c
Shrestha Hardia <84022759+hardiashrestha@users.noreply.github.com>
2024-12-16 08:57:06 +0000

    new UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Navbar.css'; 
-import logo from '../assets/STEM-INSPIRES-LOGO.png'
+import logo1 from '../assets/STEM-INSPIRES-LOGO.png'; // First logo image
+import logo2 from '../assets/STEM-INSPIRES-LOGO.png'; // Second logo image
+import logo3 from '../assets/STEM-INSPIRES-LOGO.png'; // Third logo image
+import '../index.css';
+
+const logos = [logo1, logo2, logo3];
+
 const Navbar = () => {
+    const [currentLogoIndex, setCurrentLogoIndex] = useState(0);
+    const [isMenuOpen, setIsMenuOpen] = useState(false); 
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentLogoIndex((prevIndex) => (prevIndex + 1) % logos.length);
+        }, 3000);
+
+        return () => clearInterval(interval); 
+    }, []);
+
     return (
         <div className='navbar'>
             <Link to='/' className='logo'>
-                <img src={logo} alt="Home" style={{ width: '105px', height: '97.22px', position: 'absolute', left: '3.06px', top: '4.56px' }} />
+                <img src={logos[currentLogoIndex]} alt="Home" className="logo-image" />
             </Link>
-            <Link to='/about-us' className='button about_us'>TEAM</Link>
-            <Link to='/join-us' className='button join_us'>JOIN US</Link>
-            <Link to='/events' className='button events'>EVENTS</Link>
-            <Link to='/contact' className='button contact'>CONTACT</Link>
-            <Link to='/projects' className='button projects'>PROJECTS</Link>
-            <Link to='/donate' className='button donate'>DONATE</Link>
+            <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+                <Link to='/about-us' className='button about_us'>TEAM</Link>
+                <Link to='/join-us' className='button join_us'>JOIN US</Link>
+                <Link to='/events' className='button events'>EVENTS</Link>
+                <Link to='/contact' className='button contact'>CONTACT</Link>
+                <Link to='/projects' className='button projects'>PROJECTS</Link>
+                <Link to='/donate' className='button donate'>DONATE</Link>
+            </div>
+            <div className='hamburger' onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                {/* Hamburger icon */}
+                <span className={`bar ${isMenuOpen ? 'toggle' : ''}`}></span>
+                <span className={`bar ${isMenuOpen ? 'toggle' : ''}`}></span>
+                <span className={`bar ${isMenuOpen ? 'toggle' : ''}`}></span>
+            </div>
         </div>
     );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,65 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
-import image1 from '../assets/image1.png'; 
-import image2 from '../assets/image2.png'; 
-import image3 from '../assets/image3.png'; 
+import logo1 from '../assets/STEM-INSPIRES-LOGO.png'; // First logo image
+import logo2 from '../assets/STEM-INSPIRES-LOGO.png'; // Second logo image
+import logo3 from '../assets/STEM-INSPIRES-LOGO.png'; // Third logo image
+import bgImage1 from '../assets/Home-img-0.png'; // First background image
+import bgImage2 from '../assets/Home-img-3.png'; // Second background image
+import bgImage3 from '../assets/Home-img-5.png'; // Third background image
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+import { FaInstagram, FaEnvelope, FaLinkedin } from 'react-icons/fa';
 
+const logos = [logo1, logo2, logo3];
+const backgrounds = [bgImage1, bgImage2, bgImage3];
 const Home = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-    };
+    const [currentLogoIndex, setCurrentLogoIndex] = useState(0);
+    const [currentBackgroundIndex, setCurrentBackgroundIndex] = useState(0);
+
+    useEffect(() => {
+        const logoInterval = setInterval(() => {
+            setCurrentLogoIndex((prevIndex) => (prevIndex + 1) % logos.length);
+        }, 3000); // Change logo every 3 seconds
+
+        const bgInterval = setInterval(() => {
+            setCurrentBackgroundIndex((prevIndex) => (prevIndex + 1) % backgrounds.length);
+        }, 9000); // Change background every 9 seconds
+
+        return () => {
+            clearInterval(logoInterval);
+            clearInterval(bgInterval);
+        }; // Clear intervals on component unmount
+    }, []);
 
     return (
-        <div style={{ paddingBottom: '60px' }}>
+        <div className="home-container" style={{ backgroundImage: `url(${backgrounds[currentBackgroundIndex]})` }}>
+            {/* Background Overlay */}
+            <div className="background-overlay"></div>
+
+            {/* Welcome Section */}
             <h1 className="main-header">WELCOME TO STEM-INSPIRES</h1>
-            <p className="sub-header">STEM Inspires is a student-run non-profit organization that promotes STEM learning amongst youth from underrepresented and underserved communities, building the next generation of STEM-inspired individuals.</p>
+            <p className="sub-header">
+            STEM Inspires is a student-run non-profit organization that promotes STEM learning amongst youth from underrepresented and underserved communities, building the next generation of STEM-inspired individuals.
+            </p>
 
-            <Slider {...settings} style={{ width: '475px', height: '480.22px', position: 'absolute', left: '885.06px', top: '100.56px' }}>
-                <div>
-                    <img src={image1} alt="Slide 1" style={{ width: '475px', height: '480.22px' }} />
-                </div>
-                <div>
-                    <img src={image2} alt="Slide 2" style={{ width: '475px', height: '480.22px' }} />
-                </div>
-                <div>
-                    <img src={image3} alt="Slide 3" style={{ width: '475px', height: '480.22px' }} />
-                </div>
-            </Slider>
-
-            <Link to="/about-us" className="button about-us">TEAM</Link>
-            <Link to="/contact" className="button contact-us">CONTACT</Link>
-
+            {/* Buttons Section */}
+            <div className="button-container">
+                <Link to="/about-us" className="home-button about-us">ABOUT US</Link>
+                <Link to="/contact" className="home-button contact-us">CONTACT</Link>
+            </div>
+            <div className="social-icons">
+          <a href="mailto:bookhubtindia@gmail.com" target="_blank" rel="noopener noreferrer">
+            <FaEnvelope />
+          </a>
+          <a href="https://instagram.com/bookk_hubbb" target="_blank" rel="noopener noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://www.linkedin.com/company/bookhubt/mycompany/" target="_blank" rel="noopener noreferrer">
+            <FaLinkedin />
+          </a>
+        </div>
         </div>
     );
 };
